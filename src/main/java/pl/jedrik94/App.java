181e1d827add2157b6645e49ee33e7b00f049ea4
Wojtkowiak Jedrzej <jedrik94@gmail.com>
2018-12-07 12:18:59 +0100

    Basic hibernate config file (connection to local db).

--- src/main/java/pl/jedrik94/App.java
+++ src/main/java/pl/jedrik94/App.java
@@ -1,9 +1,6 @@
 package pl.jedrik94;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
     }
 }

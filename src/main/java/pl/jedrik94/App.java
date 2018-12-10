36edb49dee9fdba9d3d89423e09cecaba57e69b4
Wojtkowiak Jedrzej <jedrik94@gmail.com>
2018-12-10 13:37:06 +0100

    Saving and retrieving data from db.

--- src/main/java/pl/jedrik94/App.java
+++ src/main/java/pl/jedrik94/App.java
@@ -7,24 +7,36 @@ import pl.jedrik94.model.Student;
 
 public class App {
     public static void main(String[] args) {
-        SessionFactory factory = new Configuration().
-                configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Student.class)
-                .buildSessionFactory();
-
-        Session session = factory.getCurrentSession();
 
         Student student = new Student();
         student.setFirstName("Zbyszek");
         student.setLastName("Kowal");
         student.setEmail("zbycho_69@gmail.com");
 
-        session.beginTransaction();
+        System.out.println(student);
 
-        session.save(student);
+        try (SessionFactory factory = new Configuration().
+                configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory()) {
 
-        session.getTransaction().commit();
+            Session session = factory.getCurrentSession();
 
-        factory.close();
+            session.beginTransaction();
+            session.save(student);
+            session.getTransaction().commit();
+
+            System.out.println("Saved student id: " + student.getId());
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            System.out.println("Retrieving student with id: " + student.getId());
+
+            Student sameStudent = session.get(Student.class, student.getId());
+            session.getTransaction().commit();
+
+            System.out.println(sameStudent);
+        }
     }
 }
